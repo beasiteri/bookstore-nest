b3c3f35cdbf3b84a18f1ad0da3004704c75ee4c0
beasiteri <siteribea@gmail.com>
2022-07-15 16:51:38 +0200

    Update book component with input field for edit mode

--- client/src/app/books/book/book.component.ts
+++ client/src/app/books/book/book.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class BookComponent implements OnInit {
   bookId: string;
   book;
+  editMode = false;
 
   constructor(private service: BooksService, private route: ActivatedRoute) { }
 
@@ -20,5 +21,24 @@ export class BookComponent implements OnInit {
       const result = [response];
       this.book = result[0]['bookById'];
     });
+  }
+
+  editBookEntity() {
+    console.log('edit');
+    this.editMode = true;
+  }
+
+  saveBookEntity() {
+    console.log('delete');
+    this.editMode = false;
+  }
+
+  cancelBookEntity() {
+    console.log('delete');
+    this.editMode = false;
+  }
+
+  deleteBookEntity() {
+    console.log('delete');
   }
 }
